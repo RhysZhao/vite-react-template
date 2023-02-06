2897f9a53c319411cd7d463e27d1684db5716615
rhys.zhao <zhaorx2012@163.com>
2023-02-06 11:05:18 +0800

    Update 修改eslint解析器为@babel/eslint-parser

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@
  * Author  rhys.zhao
  * Date  2022-11-09 18:21:27
  * LastEditors  rhys.zhao
- * LastEditTime  2022-12-27 13:18:12
+ * LastEditTime  2023-02-06 10:55:22
  * Description eslint配置
  */
 module.exports = {
@@ -21,13 +21,17 @@ module.exports = {
     "prettier"
   ],
   plugins: ["react"],
-  parser: "babel-eslint",
+  parser: "@babel/eslint-parser",
   // 解析器选项
   parserOptions: {
     sourceType: "module",
     ecmaFeatures: {
       jsx: true
-    }
+    },
+    babelOptions: {
+      presets: ["@babel/preset-react"]
+    },
+    requireConfigFile: false
   },
   settings: {
     react: {
@@ -37,7 +41,7 @@ module.exports = {
   rules: {
     "no-unused-expressions": "off",
     "no-unused-vars": "warn",
-    "no-console": "off",
+    "no-console": "warn",
     "no-debugger": "error",
     "no-unreachable": "warn",
     "react/prop-types": "warn"
