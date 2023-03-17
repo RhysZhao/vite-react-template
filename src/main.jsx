592c3945d591d32cc050c887858fcf3db413f22d
RhysZhao <1009282428@qq.com>
2023-03-17 11:06:46 +0800

    Update 增加路由basename

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
  * Author  rhys.zhao
  * Date  2022-08-24 20:11:32
  * LastEditors  rhys.zhao
- * LastEditTime  2022-12-27 10:24:43
+ * LastEditTime  2023-03-17 11:06:29
  * Description js入口文件
  */
 import React from "react";
@@ -15,7 +15,8 @@ const App = () => <>{useRoutes(routes)}</>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* todo: 这里加上basename是因为github部署路由的问题。个人部署可以不用加 */}
+    <BrowserRouter basename='/vite-react-template'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
